0c70481f25e480e11f781f2a6c3baa53b929bcc5
unknown <farrel.ahmad.zaidan@gmail.com>
2024-03-21 16:38:03 +0700

    update hero and fix navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './landing/hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
